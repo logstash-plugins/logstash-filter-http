a36e3c1d8ac695f0776a025b8fd77d927bd908eb
Tricia <95389395+asymmetricia@users.noreply.github.com>
2022-11-21 16:19:56 -0800

    skip processing for responses that do not contain a response body
    At least, HTTP 204 explicitly does not contain a response body. In this case, `body` will be NilClass, and when we call `body.strip` it'll raise an exception:
    
    ```
    :error=>"(NoMethodError) undefined method `strip' for nil:NilClass", :exception=>Java::OrgJrubyExceptions::NoMethodError, :backtrace=>["usr.share.logstash.vendor.bundle.jruby.$2_dot_6_dot_0.gems.logstash_minus_filter_minus_http_minus_1_dot_4_dot_1.lib.logstash.filters.http.process_response(/usr/share/logstash/vendor/bundle/jruby/2.6.0/gems/logstash-filter-http-1.4.1/lib/logstash/filters/http.rb:151)"
    ```

--- lib/logstash/filters/http.rb
+++ lib/logstash/filters/http.rb
@@ -135,6 +135,7 @@ EOF
     content_type, _ = headers.fetch("content-type", "").split(";")
     event.set(@target_headers, headers)
     return if @verb == 'head' # Since HEAD requests will not contain body, we need to set only header
+    return if body.nil?       # Catch other responses (HTTP 204?) that also do not contain bodies
     if content_type == "application/json"
       begin
         parsed = LogStash::Json.load(body)
